2cdf7e522f1536ec20e85b3491cc1b9893deb083
manuel <manuel@DESKTOP-3LFEVQQ.localdomain>
2022-09-30 05:41:35 -0300

    paginado basico 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Layout  from "./pages/Layout";
+import About  from "./pages/About";
+import Home  from "./pages/Home";
+import Default  from "./pages/Default";
+import Dashboard  from "./pages/Dashboard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Routes</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+           <Route path="about" element={<About />} />
+           <Route path="/" element={<Home />} />
+           <Route path="dashboard" element={<Dashboard />} />
+           <Route path="*" element={<Default />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
